c33900878d865922dad01b2dd5bfab38755533b0
jessica <jragland1@live.maryville.edu>
2018-02-06 23:23:10 -0500

    Update

--- InterfaceBuilderBasics/ViewController.swift
+++ InterfaceBuilderBasics/ViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func buttonPressed(_ sender: Any) {
+   print("The button was pressed") }
+    @IBOutlet weak var myButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myButton.setTitleColor(.red, for: .normal)
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
